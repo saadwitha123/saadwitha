6ffcafe66d35dd4a9b472bfebd89d885031217f8
saadwitha <vsaadwitha@gmail.com>
2025-09-17 12:01:03 +0530

    adding datatypes

---
+++ datatypes_new.py
@@ -0,0 +1,32 @@
+#integer
+a=10
+print("a=",a,"| Type:",type(a))
+
+#float
+b=3.14
+print("b=",b,"|Type:",type(b))
+
+#string
+c="Hello"
+print("c=",c,"|Type:", type(c))
+
+#boolean
+d=True
+print("d=",d,"|Type:", type(d))
+
+#list
+e=[1,2,3,"apple"]
+print("e=",e,"|Type:", type(e))
+
+#tuple
+f=(1,2,3)
+print("f=",f,"|Type:", type (f))
+
+#set
+g={1,2,2,3}
+print("g=",g,"|Type:", type(g))
+
+#dictionary
+h={"name":"alice","age":25}
+print("h=",h,"|Type:", type(h))
+
