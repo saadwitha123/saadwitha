c6d287eb0d639baf0b5f8b506075b4e8e822b623
saadwitha <vsaadwitha@gmail.com>
2025-09-17 12:12:55 +0530

    adding dictionaries

---
+++ dictionary_new.py
@@ -0,0 +1,11 @@
+student = {
+  "name" : "saadwitha",
+  "age" : 20,
+  "course":"python",
+  "marks":95
+}
+
+print("Name:",student["name"])
+print("age:",student["age"])
+print("course:",student["course"])
+print("marks:",student["marks"])
