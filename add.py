39d400a23585ff2e41f479dc0e5cd99f4e62ce19
saadwitha <vsaadwitha@gmail.com>
2025-09-17 11:46:19 +0530

    adding add.py

---
+++ add.py
@@ -0,0 +1,14 @@
+x = True
+y = True
+resultAND =x and y 
+resultOR =x or y
+if resultAND:
+    print("result is true")
+if resultOR:
+    print("result1 is true")
+#not   
+z = False
+if not z:
+   print("result2 is true")
+else:
+    print("result3 is false")
